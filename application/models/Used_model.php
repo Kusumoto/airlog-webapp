e40540451df7192e644b4f8fc31af6f1f42af5b4
Weerayut Hongsa <kusumoto.com@gmail.com>
2016-02-10 02:02:27 +0700

    Fix graphmonthused_dashboard in Used_model to support highchart.

--- application/models/Used_model.php
+++ application/models/Used_model.php
@@ -704,6 +704,7 @@ class Used_model extends CI_Model {
  	{
  		// announce return variable
  		$return 	= 	array();
+ 		$count 		=	array();
  		// select logger collection 
  		$use_data 	= 	$this->mongo_db->db->used;
  		// preparing query data
@@ -719,10 +720,7 @@ class Used_model extends CI_Model {
  					'use_date' 	=> 	$splitday[0].'-'.$splitday[1].'-0'.$i
  					);
  				// count and insert data to array
- 				$return[] 	= 	array(
- 					'date' 		=> 	$splitday[0].'-'.$splitday[1].'-0'.$i, 
- 					'total' 	=> 	$use_data->count($querydata)
- 					);
+ 				array_push($count, $use_data->count($querydata));
  			} 
  			else 
  			{
@@ -731,12 +729,15 @@ class Used_model extends CI_Model {
  					'use_date' 	=> 	$splitday[0].'-'.$splitday[1].'-'.$i
  					);
  				// count and insert data to array
- 				$return[] 	= 	array(
- 					'date' 	 	=> 	$splitday[0].'-'.$splitday[1].'-'.$i, 
- 					'total' 	=> 	$use_data->count($querydata)
- 					);
+ 				array_push($count, $use_data->count($querydata));
  			}
  		}
+ 		
+ 		$return		=	array(
+ 			'name'		=>		'User',
+ 			'data'		=>		$count
+ 			);
+
  		return $return;	
  	}
 
