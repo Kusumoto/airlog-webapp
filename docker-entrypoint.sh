560bbc6404c3d378f30e35677163d0556d9f22f0
Weerayut Hongsa <kusumoto.com@gmail.com>
2016-02-19 00:41:07 +0700

    Update shell script for docker install.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,11 @@ FILE="/var/www/html/install.lock"
 
 if [ -f "$FILE" ];
 then
-
-php /var/www/html/index.php installation docker_step1 $MONGO_HOST $MONGO_PORT $MONGO_USER $MONGO_PWD $MONGO_DB
-
-php /var/www/html/index.php installation docker_step2 $SAMF_USER $SAMF_PWD $SAMF_NAMEF $SAMF_NAMEL
-
-touch $FILE
+	exec apache2-foreground
+else
+	php /var/www/html/index.php installation docker_step1 $MONGO_HOST $MONGO_PORT $MONGO_USER $MONGO_PWD $MONGO_DB
+	php /var/www/html/index.php installation docker_step2 $SAMF_USER $SAMF_PWD $SAMF_NAMEF $SAMF_NAMEL	
+	touch $FILE
+	exec apache2-foreground
 fi
 
-exec apache2-foreground
