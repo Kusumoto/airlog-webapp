3d1d23f53a4fa652d25e658dcfa1d82b371aefc0
Muhot007 <muhot007@hotmail.com>
2016-02-20 01:51:54 +0700

    edit

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -88,7 +88,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array("multilang");
+$autoload['helper'] = array('multilang');
 // The multilange helper is helper for multi language
 
 /*
