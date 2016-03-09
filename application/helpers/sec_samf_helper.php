0f7792e597aa8cb76834cc038a1e4be58c42acf4
Weerayut Hongsa <kusumoto.com@gmail.com>
2016-03-09 22:37:25 +0700

    Fix SAMF Security fix.

--- application/helpers/sec_samf_helper.php
+++ application/helpers/sec_samf_helper.php
@@ -1,2 +1,23 @@
-<?php function sec_samf()
-	{ } ?>
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* SAMF Security Checker
+*
+* @author Weerayut Hongsa <kusumoto.com@gmail.com>
+* @copyright Copyright (c) 2014, Weerayut Hongsa, http://kusumotolab.com
+* @version 1.0.0
+*/
+
+if ( ! function_exists('sec_samf'))
+{
+	function sec_samf()
+	{
+		// Check Root Path Can Writable
+		if (is_writable(FCPATH))
+			show_error('Security: Path '.FCPATH.' can writable, fix it.',500);
+		
+		// Check Configure Path Can Writable
+		if (is_writable(APPPATH.'config/'))
+			show_error('Security: Path '.APPPATH.'config/'.' can writable, fix it.',500);
+	}
+}
